340c2ac4b9c79e6073ded6ca74fe903848e92996
Mads Moi-Aune <mads.aune@rejlers.no>
2019-12-06 10:32:17 +0100

    Altered to work with AWS Lambda

--- README.md
+++ README.md
@@ -1,13 +1,37 @@
 This script fetches the lunch menu from deg16.no and publish it to a channel in Teams.
 
-1. Install nodejs and git
-2. Clone this repo to `/srv`
-3. Run `npm install`
-4. Add a file called `.env` and add your Teams Inomming Webhook URL to the variable TEAMS_URL
-5. Add this to CRONTAB
+This script has now moved to AWS Lambda.
 
-crontab
+## Installation instructions
 
-    sudo su
-    crontab -e
-    00 10 * * 1-5 /usr/local/bin/node /srv/deg16-lunchmenu-teams/index.js
+```bash
+git clone https://github.com/rejlersembriq/deg16-lunchmenu-teams.git
+cd deg16-lunchmenu-teams
+
+# Permissions are needed for AWS to be able to read and execute our modules
+chmod -R 777 node_modules
+chmod 777 index.js
+
+zip -r function.zip .
+```
+
+### Setup AWS Lambda function
+1. Create a new AWS Lambda function
+2. Set runtime to be > nodejs8
+3. In the "Code entry type", choose "Upload a .zip file"
+4. Upload `function.zip` you created earlier
+5. Hit "Save"
+6. Make sure Handler is `index.handler`
+7. Under "Environmental Variables" enter "TEAMS_URL" for name and then the URL to your Incomming Webhook
+
+### Setup trigger via CloudWatch
+4. Go to CloudWatch/Rules and "Create rule"
+5. Event Source must be "Scehduled"
+6. Choose "Cron expression" with this expression `0 10 ? * MON-FRI *`
+5. Click "Add Target" and choose "Lambda function"
+6. Select your AWS Lambda function
+7. Click "Configure Details"
+
+## Updating AWS Lambda function
+
+For updating an already existing function you just have to zip your files again and upload using "Upload a .zip file".
--- index.js
+++ index.js
@@ -1,38 +1,39 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 const dateFormat = require('dateformat');
-require('dotenv').config();
 
 const websiteUrl = 'https://braatheneiendom.no/eiendommer-5190/dronning-eufemias-gate-16/praktisk-informasjon';
 const today = new Date().getDay();
 const weekdays = [ "SØNDAG", "MANDAG", "TIRSDAG", "ONSDAG", "TORSDAG", "FREDAG", "LØRDAG" ];
 
-axios.get(websiteUrl)
-    .then(resp => {
 
-        const $ = cheerio.load(resp.data);
-        const menu = $('.blocktext .abstract').text().trim().split('\n');
-        
-        // Chop off everything before today's entries
-        menu.splice(0, menu.indexOf(weekdays[today]));
-        
-        // If its friday (and since its no saturday)
-        // we need to chop off the rest of the menu based on another criteria
-        if (today == 5) {
-            menu.splice(menu.indexOf('BESTILLING AV MAT TIL MØTER'));
-        } else {
-            menu.splice(menu.indexOf(weekdays[today+1]));
-        }
-        
-        let title = menu.splice(0, 1)[0].trim() + " ";
-        title += dateFormat(new Date(), 'dd mmm yyyy').toUpperCase();
-        
-        let text;
-        text = title + "\n\n";
-        text += "---\n\n";
-        text += menu.join("\n\n");
-
-        axios.post(process.env.TEAMS_URL, { text })
+exports.handler = function(event, context) {
+    axios.get(websiteUrl)
+        .then(resp => {
+    
+            const $ = cheerio.load(resp.data);
+            const menu = $('.blocktext .abstract').text().trim().split('\n');
+            
+            // Chop off everything before today's entries
+            menu.splice(0, menu.indexOf(weekdays[today]));
+            
+            // If its friday (and since its no saturday)
+            // we need to chop off the rest of the menu based on another criteria
+            if (today == 5) {
+                menu.splice(menu.indexOf('BESTILLING AV MAT TIL MØTER'));
+            } else {
+                menu.splice(menu.indexOf(weekdays[today+1]));
+            }
+            
+            let title = menu.splice(0, 1)[0].trim() + " ";
+            title += dateFormat(new Date(), 'dd mmm yyyy').toUpperCase();
+            
+            let text;
+            text = title + "\n\n";
+            text += "---\n\n";
+            text += menu.join("\n\n");
+    
+            axios.post(process.env.TEAMS_URL, { text })
             .then(resp => {
                 if (resp.status !== 200) {
                     console.log(resp.statusText);
@@ -42,3 +43,4 @@ axios.get(websiteUrl)
 
     })
     .catch(err => console.log(err));
+}
